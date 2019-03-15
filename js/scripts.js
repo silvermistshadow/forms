260cf26c0cf82896bc211b57dfa766d17f8b454e
silvermistshadow <bmead72@gmail.com>
2019-03-15 10:40:55 -0700

    redo javascript to use arrays and looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,14 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-      var person1Input = $("input#person1").val();
-      var person2Input = $("input#person2").val();
-      var animalInput= $("input#animal").val();
-      var exclamationInput = $("input#exclamation").val();
-      var verbInput = $("input#verb").val();
-      var nounInput = $("input#noun").val();
-
-      $(".person1").text(person1Input);
-      $(".person2").text(person2Input);
-      $(".animal").text(animalInput);
-      $(".exclamation").text(exclamationInput);
-      $(".verb").text(verbInput);
-      $(".noun").text(nounInput);
-
-      $("#story").show();
-
-
     event.preventDefault();
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+    $("#story").show();
+
   });
 });
